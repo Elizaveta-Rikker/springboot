b3939f37f34a13ecce5db1ea807da81957296414
Liza Rikker <lrikker@yandex.ru>
2022-04-28 19:36:59 +0300

    solid

--- src/main/java/org/rikker/springboot/controllers/UsersController.java
+++ src/main/java/org/rikker/springboot/controllers/UsersController.java
@@ -2,13 +2,13 @@ package org.rikker.springboot.controllers;
 
 import org.rikker.springboot.models.User;
 import org.rikker.springboot.services.UserService;
+import org.rikker.springboot.services.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -23,7 +23,7 @@ public class UsersController {
     private final UserService userService;
 
     @Autowired
-    public UsersController(UserService userService) {
+    public UsersController(UserServiceImpl userService) {
         this.userService = userService;
     }
 
--- src/main/java/org/rikker/springboot/services/UserService.java
+++ src/main/java/org/rikker/springboot/services/UserService.java
@@ -1,38 +1,21 @@
 package org.rikker.springboot.services;
 
-
 import org.rikker.springboot.models.User;
 import org.rikker.springboot.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class UserService {
-    private final UserRepository userRepository;
+public interface UserService {
 
-    @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    public List<User> index() {
-        return userRepository.findAll();
-    }
+    public List<User> index();
 
 
-    public User show(int id) {
-        return userRepository.getById(id);
-    }
+    public User show(int id);
 
 
-    public void save(User user) {
-        userRepository.save(user);
-    }
+    public void save(User user);
 
 
-    public void delete(int id) {
-        userRepository.deleteById(id);
-    }
+    public void delete(int id);
 }
